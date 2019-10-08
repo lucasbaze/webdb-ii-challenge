c4c9936cee74af226260331dfe7b854f405b63b3
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-08 14:29:42 -0500

    added in error handling query

--- index.js
+++ index.js
@@ -10,5 +10,9 @@ server.get('/', (req, res) => {
     res.send('Server up and running!');
 });
 
+server.use((err, req, res, next) => {
+    res.status(400).json(err);
+});
+
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => `Server is running on PORT: ${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,23 @@ const config = require('../knexfile');
 
 const db = knex(config.development);
 
+//
+//Get all cars
 router.get('/', async (req, res) => {
     let cars = await db('cars');
     res.status(200).json(cars);
+});
+
+//
+//Create a new car
+router.post('/', async (req, res, next) => {
+    let { vin, make, model, milage } = req.body;
+
+    if (!vin || !make || !modal || !milage) {
+        next('Missing parameter');
+    }
+
+    let newCar = await db.table('cars').insert({});
 });
 
 module.exports = router;
